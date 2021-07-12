f99f7e524f0afcfb97d6598b32effdde4f76d189
Smsoft007 <muindo25@gmail.com>
2021-07-12 16:01:28 +0900

    Updates

---
+++ precache-manifest.6c25e588060c67938070d7a18f5f89a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc71270f8c600df4107362880863fe11",
+    "url": "/react_basic_habit_tracker/index.html"
+  },
+  {
+    "revision": "086bda339cd7ba27eca2",
+    "url": "/react_basic_habit_tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/react_basic_habit_tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react_basic_habit_tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086bda339cd7ba27eca2",
+    "url": "/react_basic_habit_tracker/static/js/main.3443b86c.chunk.js"
+  },
+  {
+    "revision": "cce1584d664dd6fcc7a5",
+    "url": "/react_basic_habit_tracker/static/js/runtime-main.929c0c59.js"
+  }
+]);
